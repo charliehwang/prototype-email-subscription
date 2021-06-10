6b8b6468184fd2d630a7b306a724e8538a8593ab
charlie <charliehwang@gmail.com>
2021-06-10 10:57:45 -0600

    Lambda fn added

--- lib/email-subscription-stack.ts
+++ lib/email-subscription-stack.ts
@@ -2,22 +2,20 @@ import * as sns from "@aws-cdk/aws-sns";
 import * as subs from "@aws-cdk/aws-sns-subscriptions";
 import * as sqs from "@aws-cdk/aws-sqs";
 import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
 
 export class EmailSubscriptionStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // we are going to call this function via APIGatewayEvent which is used for http requests
-    exports.handler = async function (event: AWSLambda.APIGatewayEvent) {
-      // this is a neat trick to prettify the console log
-      console.log("request:", JSON.stringify(event, null, 2));
-
-      // this is what calling this lambda function will return
-      return {
-        statusCode: 200,
-        headers: { "Content-Type": "text/plain" },
-        body: `Hello, egghead friends!`,
-      };
-    };
+    const helloLambda = new lambda.Function(this, "HelloLambda", {
+      // where our code is located (inside the lambda directory)
+      code: lambda.Code.fromAsset("lambda"),
+      // the function executed whenever this lambda function is triggered (the handler function inside hello.ts file)
+      handler: "hello.handler",
+      // most recent node
+      runtime: lambda.Runtime.NODEJS_12_X,
+      environment: { isProduction: "absolutely not" },
+    });
   }
 }
