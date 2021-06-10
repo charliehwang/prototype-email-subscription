21dfb10acbb1c7ee99e08bdbcfb8a27e305306e3
charlie <charliehwang@gmail.com>
2021-06-10 07:42:33 -0600

    first checkin

--- lib/email-subscription-stack.ts
+++ lib/email-subscription-stack.ts
@@ -1,18 +1,23 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
-import * as cdk from '@aws-cdk/core';
+import * as sns from "@aws-cdk/aws-sns";
+import * as subs from "@aws-cdk/aws-sns-subscriptions";
+import * as sqs from "@aws-cdk/aws-sqs";
+import * as cdk from "@aws-cdk/core";
 
 export class EmailSubscriptionStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'EmailSubscriptionQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
+    // we are going to call this function via APIGatewayEvent which is used for http requests
+    exports.handler = async function (event: AWSLambda.APIGatewayEvent) {
+      // this is a neat trick to prettify the console log
+      console.log("request:", JSON.stringify(event, null, 2));
 
-    const topic = new sns.Topic(this, 'EmailSubscriptionTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+      // this is what calling this lambda function will return
+      return {
+        statusCode: 200,
+        headers: { "Content-Type": "text/plain" },
+        body: `Hello, egghead friends!`,
+      };
+    };
   }
 }
